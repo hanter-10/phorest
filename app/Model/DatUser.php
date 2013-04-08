0d746c9bdbaeb45589f5abcf8ce1c7fe4e7ed3f0
hanter-10 <shori.muraoka@gmail>
2013-04-09 01:08:58 +0900

    merge commit

--- app/Model/DatUser.php
+++ app/Model/DatUser.php
@@ -59,10 +59,7 @@ class DatUser extends AppModel {
 			'sitename' => array(
 					'notempty' => array(
 							'rule' => array('notEmpty'),
-							'message' => 'サイト名を入力してください'),
-					'minlength' => array(
-							'rule' => array('minLength', '5'),
-							'message' => 'サイト名は5文字以上で入力してください。')),
+							'message' => 'サイト名を入力してください')),
 			);
 
 	//The Associations below have been created with all possible keys, those that are not needed can be removed
